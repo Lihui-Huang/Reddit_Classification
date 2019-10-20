c724ad180eb78bc6da5d9ae533e3e4e891db2903
IanWangg <43913458+IanWangg@users.noreply.github.com>
2019-10-20 12:05:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from nltk import WordNetLemmatizer
 from nltk import word_tokenize
 import feature_extraction
 import models.models as model
+from sklearn.metrics import accuracy_score
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -13,11 +14,23 @@ import numpy as np
 
 logreg = model.LogReg()
 
+NB = model.Bernoulli()
+
 SVC = model.LSVC()
 
 Ensemble = model.ensemble()
 
 MultiNB = model.MultinomialNB()
+
+X, y = feature_extraction.pre_process_comments()  # get the data
+
+BNB = model.NB(X, y)
+
+BNB_pred = BNB.predict(X)
+
+MultiNB_acc = accuracy_score(y_pred=BNB_pred, y_true=y)
+
+
 
 
 
